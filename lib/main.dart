053a09070655b21785385cce444266721df92b1a
Guru <mguruprasanth2004@gmail.com>
2023-09-17 18:12:30 +0530

    Map custom page added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:sih_2023/features/model/firebase_api.dart';
 import 'package:sih_2023/features/theme/build_theme.dart';
+import 'package:sih_2023/features/ui/responsehub/view/map_test.dart';
 import 'features/ui/onboarding/view/signin.dart';
 import 'features/ui/onboarding/view/register.dart';
 import 'firebase_options.dart';
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
             if (snapshot.data == null) {
               return const SignInPage();
             } else {
-              return const RegistrationPage();
+              return WidgetToMarker();
             }
           }
           return const Center(
