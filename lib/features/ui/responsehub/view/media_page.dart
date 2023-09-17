e7647ca9f737af551833c9296029a7e0379d6708
Guru <mguruprasanth2004@gmail.com>
2023-09-18 00:15:48 +0530

    bugsfixed

--- lib/features/ui/responsehub/view/media_page.dart
+++ lib/features/ui/responsehub/view/media_page.dart
@@ -3,14 +3,29 @@ import 'package:flutter/material.dart';
 class MediaPage extends StatefulWidget {
   final String roomName;
 
-   MediaPage({required this.roomName});
+  MediaPage({required this.roomName});
 
   @override
   _MediaPageState createState() => _MediaPageState();
 }
 
-class _MediaPageState extends State<MediaPage> {
-  int _selectedOption = 0;
+class _MediaPageState extends State<MediaPage> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,56 +40,25 @@ class _MediaPageState extends State<MediaPage> {
         ),
         title: Text(widget.roomName),
         centerTitle: true,
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(45.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              _buildTabOption(0, 'Medias'),
-              _buildTabOption(1, 'Docs'),
-              _buildTabOption(2, 'Links'),
-            ],
-          ),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Medias'),
+            Tab(text: 'Docs'),
+            Tab(text: 'Links'),
+          ],
+          indicatorColor: Colors.blueAccent,
+          labelColor: Colors.blueAccent,
+          unselectedLabelColor: Colors.black,
         ),
       ),
-      body: Column(
-        children: [
-          if (_selectedOption == 0)
-            const Center(child: Text('Medias')),
-          if (_selectedOption == 1)
-            const Center(child: Text('Docs')),
-          if (_selectedOption == 2)
-            const Center(child: Text('Links')),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+           Center(child: Text('Medias')),
+           Center(child: Text('Docs')),
+           Center(child: Text('Links')),
         ],
-      ),
-    );
-  }
-
-  Widget _buildTabOption(int index, String title) {
-    final isSelected = index == _selectedOption;
-
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _selectedOption = index;
-        });
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        decoration: isSelected
-            ? const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(width: 5, color: Colors.blueAccent),
-                ),
-              )
-            : null,
-        child: Text(
-          title,
-          style: TextStyle(
-            color: isSelected ? Colors.blueAccent : Colors.black,
-            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-          ),
-        ),
       ),
     );
   }
