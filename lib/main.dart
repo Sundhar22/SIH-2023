bdffaa70a1cb06b499d572ff22976f7218eec166
Guru <mguruprasanth2004@gmail.com>
2023-09-18 14:38:37 +0530

    profile page added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:sih_2023/features/model/firebase_api.dart';
 import 'package:sih_2023/features/theme/build_theme.dart';
+import 'package:sih_2023/features/ui/Info/view/profile_info.dart';
 import 'package:sih_2023/features/ui/onboarding/view/register.dart';
 import 'features/ui/onboarding/view/signin.dart';
 import 'firebase_options.dart';
