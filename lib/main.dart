3e83b4432c3a36ac282f6ff4b3318e2c68a12b92
Sundhar22 <mohanasundharamofficial@gmail.com>
2023-12-13 13:41:09 +0530

    conflict removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 // import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:sih_2023/features/model/firebase_api.dart';
 import 'package:sih_2023/features/theme/build_theme.dart';
 import 'package:sih_2023/features/ui/home/view/home.dart';
-import 'package:sih_2023/test.dart';
 
 import 'firebase_options.dart';
 
@@ -26,6 +25,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'SIH-23',
         theme: buildTheme(),
-        home: Home);
+        home: const HomeScreen());
   }
 }
